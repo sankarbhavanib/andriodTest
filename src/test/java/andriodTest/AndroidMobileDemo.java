f3d53da9538ce178917a29f9ae79ae45218894c8
Bhavani Bheemanadham <bbheemanadham@paypal.com>
2016-02-29 13:43:50 -0600

    first commit

---
+++ src/test/java/andriodTest/AndroidMobileDemo.java
@@ -0,0 +1,33 @@
+package andriodTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.paypal.selion.annotations.MobileTest;
+import com.paypal.selion.platform.grid.Grid;
+import com.paypal.selion.reports.runtime.SeLionReporter;
+
+/*
+ * A Selendroid test case example for Native App using SeLion.
+ */
+public class AndroidMobileDemo {
+
+  @Test
+  @MobileTest(appName = "io.selendroid.testapp", device = "android:19", 
+    mobileNodeType = "selendroid")
+  public void testMethod() throws Exception {
+    SeLionReporter.log("My Screenshot 1", true);
+    WebElement inputField = Grid.driver().findElement(
+        By.id("my_text_field"));
+
+    // verify TextField is enabled
+    Assert.assertEquals("true", inputField.getAttribute("enabled"));
+    SeLionReporter.log("My Screenshot 2", true);
+    inputField.sendKeys("Selendroid");
+
+    // verify content in the TextField
+    Assert.assertEquals("Selendroid", inputField.getText());
+  }
+}
